6043fb0d228823f675cc5ba80269f07f722cbe9e
Renataclara <renataclarak@gmail.com>
2017-07-25 11:11:06 +0700

    done

---
+++ latihan.js
@@ -0,0 +1,8 @@
+var sports = ['soccer', 'baseball'];
+sports.push('football', 'swimming');
+
+console.log(sports); // ['soccer', 'baseball', 'football', 'swimming']
+// console.log(total);  // 4
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.push("Kiwi"));
